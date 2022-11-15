a2270394ae23f62a91b1eff85d7886c94571e54c
Maxim Wilhelm <maxim-wilhelm@hotmail.com>
2022-11-15 23:34:31 +0300

    Update Location.java

--- src/main/java/ru/netology/entity/Location.java
+++ src/main/java/ru/netology/entity/Location.java
@@ -5,11 +5,8 @@ import java.util.Objects;
 public class Location {
 
     private final String city;
-
     private final Country country;
-
     private final String street;
-
     private final int builing;
 
     public Location(String city, Country country, String street, int builing) {
